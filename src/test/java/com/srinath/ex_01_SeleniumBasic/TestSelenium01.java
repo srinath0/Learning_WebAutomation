a59603c429a064bb5afe268304f2e36bb40f09df
srinath0 <keshetti.srinath@gmail.com>
2025-06-18 22:33:04 +0530

    Selenium Part1

---
+++ src/test/java/com/srinath/ex_01_SeleniumBasic/TestSelenium01.java
@@ -0,0 +1,16 @@
+package com.srinath.ex_01_SeleniumBasic;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class TestSelenium01 {
+    //Write the code which will perfor UI interactions
+    @Test
+    public void test_VerifyVWOLogin() {
+        FirefoxDriver driver = new FirefoxDriver();
+        driver.get("https://app.vwo.com");
+        System.out.println(driver.getTitle());
+        driver.quit();
+    }
+
+}
